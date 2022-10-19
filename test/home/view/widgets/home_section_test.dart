39cda1ea8bb30e394c7ee38e2ec41d6816a9796a
Carl Duncan <carl.duncan@gkco.com>
2022-10-18 19:12:51 -0500

    tests: (home) added more unit tests

--- test/home/view/widgets/home_section_test.dart
+++ test/home/view/widgets/home_section_test.dart
@@ -2,7 +2,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:journal/home/widgets/home_entry_tile.dart';
 import 'package:journal/home/widgets/home_section.dart';
+import 'package:journal_api/journal_api.dart';
 import '../../../helpers/helpers.dart';
 
 void main() {
@@ -10,16 +12,28 @@ void main() {
     testWidgets('renders Text', (tester) async {
       await tester.pumpApp(
         CustomScrollView(
-          slivers: const [
+          slivers: [
             HomeSection(
               title: 'Test Title',
-              entries: [],
+              entries: [
+                Entry(
+                  id: 1,
+                  title: 'Test Title',
+                  body: 'Test Body',
+                  createdAt: DateTime.now(),
+                  updatedAt: DateTime.now(),
+                  userId: '1234567890',
+                ),
+              ],
             ),
           ],
         ),
       );
       expect(find.byType(Text), findsWidgets);
       expect(find.text('Test Title'), findsOneWidget);
+      expect(find.byType(SliverList), findsOneWidget);
+      expect(find.byType(Padding), findsOneWidget);
+      expect(find.byType(HomeEntryTile), findsWidgets);
     });
   });
 }
