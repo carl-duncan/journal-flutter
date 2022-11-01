e0f5e8285376d91ab8a55b9ba0e70fab09714517
Carl Duncan <carl.duncan@gkco.com>
2022-11-01 00:31:46 -0500

    feat: (general) added the local_auth library

--- android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.verygoodcore.journal
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
