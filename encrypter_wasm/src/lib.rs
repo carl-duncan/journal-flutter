cf7173071b2febc7f220c9a45fcb6a3ec540eefa
Carl Duncan <carl.duncan@gkco.com>
2022-11-04 10:58:52 -0500

    fix: (wasm) checking if the decrypt function had an error before returning response.

--- encrypter_wasm/src/lib.rs
+++ encrypter_wasm/src/lib.rs
@@ -12,6 +12,9 @@ impl encrypter::Encrypter for Encrypter {
     fn decodestr(message: String, key: String) -> String {
         let mc = new_magic_crypt!(key, 256);
         let base64 = mc.decrypt_base64_to_string(message);
+        if base64.is_err() {
+            return "error".to_string();
+        }
         base64.unwrap()
     }
 }
